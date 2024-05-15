5533aaf27cda1b4d72afae0616084bdb56bd4deb
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2024-05-15 15:54:11 -0400

    8 out of 11 - Formatting mission view

---
+++ Moonshot/Color-Theme.swift
@@ -0,0 +1,19 @@
+//
+//  Color-Theme.swift
+//  Moonshot
+//
+//  Created by Gabriel Marquez on 2024-05-15.
+//
+
+import Foundation
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    static var darkBackground: Color {
+        Color(red: 0.1, green: 0.1, blue: 0.2)
+    }
+
+    static var lightBackground: Color {
+        Color(red: 0.2, green: 0.2, blue: 0.3)
+    }
+}
