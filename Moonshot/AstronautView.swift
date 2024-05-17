2c2dd3e985bbbf4dbb8f8cfb95ccd0a60c7ec53b
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2024-05-16 20:06:02 -0400

    11 out of 11

---
+++ Moonshot/AstronautView.swift
@@ -0,0 +1,35 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Gabriel Marquez on 2024-05-16.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    return AstronautView(astronaut: astronauts["adrin"]!)
+        .preferredColorScheme(.dark)
+}
